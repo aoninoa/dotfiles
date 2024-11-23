4ae880603dbb34908f54d3297b42ed576a4d7f7e
aoninoa <aoninoa@proton.me>
2024-11-23 19:35:39 +0900

    Modify Telescope config

--- .config/nvim_linux/nvim/lua/plugins/telescope.lua
+++ .config/nvim_linux/nvim/lua/plugins/telescope.lua
@@ -5,17 +5,24 @@ return {
 		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-ui-select.nvim"
 		},
-		lazy = true,
+		lazy = false,
 		keys = {
 			{ mode = "n", "<leader>ff" },
 			{ mode = "n", "<leader>fg" },
 			{ mode = "n", "<leader>fb" },
-			{ mode = "n", "<leader>fl" },
+			{ mode = "n", "<leader>fs" },
+			{ mode = "n", "<leader>xq" }
 		},
 		config = function()
 			local builtin_actions = require("telescope.actions")
 			require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown()
+                    }
+                },
 				-- Default mapping
 				defaults = {
 					vimgrep_arguments = {
@@ -31,31 +38,31 @@ return {
 					mappings = {
 						-- insert mode
 						i = {
-							["<C-j>"] = builtin_actions.select_default,
+							-- ["<C-j>"] = builtin_actions.select_default,
 							["<C-o>"] = builtin_actions.select_horizontal,
 						},
 						-- normal mode
 						n = {},
 					},
 				},
-				extensions = {},
 			})
 
-			-- key mappings
-			-- Open file_finder
-			vim.keymap.set("n", "<leader>ff", function()
-				require("telescope.builtin").find_files()
-			end, {})
+            require("telescope").load_extension("ui-select")
 
-			-- Open live_grep
-			vim.keymap.set("n", "<leader>fg", function()
-				require("telescope.builtin").live_grep()
-			end, {})
-
-			-- Open buffer list
-			vim.keymap.set("n", "<leader>fb", function()
-				require("telescope.builtin").buffers()
-			end, {})
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fd", function ()
+                builtin.find_files({
+                    hidden = true,
+                    no_ignore = true,
+                })
+            end, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
+            vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
+            vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
+            vim.keymap.set("n", "<leader>ft", builtin.diagnostics, {})
 		end,
 	},
 }
